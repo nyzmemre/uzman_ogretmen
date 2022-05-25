f0416c8cbb22ed34d2f1c7f318e4184de6d2cd83
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2022-05-25 06:36:00 +0300

    added kapsayici egitim info

--- lib/view_model/info_cards/egitimkapsayicilik_info_view_model.dart
+++ lib/view_model/info_cards/egitimkapsayicilik_info_view_model.dart
@@ -4,39 +4,51 @@ import '../../model/info_card_item.dart';
 import '../../model/info_card_model.dart';
 
 class EgitimKapsayiciInfoViewModel extends ChangeNotifier{
+  List<InfoCardModel> get egitimKapsayici => _egitimKapsayiciList;}
+
   List<InfoCardModel> _egitimKapsayiciList = [
     InfoCardModel("Eğitimde Kapsayıcılık-1", [
-      InfoCardItem("egitimkapsayici_info_1", "Nedir", "Öğrencinin merkezde olduğu, öğrenci istek ve ihtiyaçlarının tespit edilmesiyle oluşturulan ve tüm öğrencileri kapsayan eğitimdir.\n-Tüm çocukların gelişimsel süreçleri dikkate alınır.\n-Eğitimde fırsat eşitliği sağlanır.\n-Öğrencinin kendi yaşantısıyla öğrenmesi esastır."),
-      InfoCardItem("egitimkapsayici_info_2", "Özellikleri", "-Öğrenci odaklıdır.\n-Bireysel özellikler dikkate alınır.\n-Öğrencilerin ihtiyaçları merkezdedir.\n-Eşitlik ilkesine dayanır.\n-Eğitime ulaşma güçlüğü çeken öğrencilere de ulaştırılır.\n-Sürece ailenin de aktif katılımı beklenir."),
-      InfoCardItem("egitimkapsayici_info_3", "Özellikleri", "-Öğrencilerin okula uyum sağlamalarını değil, okulun öğrencilere uyum sağlamalarını temel alır.\n-Cinsiyet, etnik köken, engel durumu gibi özellikleri varlığını yok sayarak eşit imkanlarda eğitim modelini savunur.\n-Ekonomik gelirin düşük olmasından kaynaklı eğitim ihtiyaçlarının giderilmesini hedefler."),
-      InfoCardItem("egitimkapsayici_info_4", "İlkeleri", "-Tüm öğrenciler eşit imkan ve şartlarda eğitim almalıdır.\n-Engeli sebebiyle eğitimden uzak kalan bireyler sürece dahil edilmelidir.\n-Toplum tarafından veya herhangi bir sebeple dışlanma riski taşımakta olan çocukların korunması gerekir.\n-Öğrencilerin ihtiyaçlarına göre etkinlikler planlanmalıdır.\n-Öğrencilerin iyi oldukları konuları açığa çıkarmalarına müsade edilir."),
-
-    ]),InfoCardModel("Hazırlanıyor", [
-      InfoCardItem("Hazırlanıyor", "Hazırlanıyor", "Hazırlanıyor"),
-      InfoCardItem("Hazırlanıyor", "Hazırlanıyor", "Hazırlanıyor"),
-      InfoCardItem("Hazırlanıyor", "Hazırlanıyor", "Hazırlanıyor"),
-    ]),
+      InfoCardItem("egitimkapsayici_info_1", "Tanım-1", "-Dışlanma, eşitsizlik ve ayrımcılık eğitim süreçleri içerisinde görülen olumsuz davranışlardandır.\n-Eğitim sistemleri ile bütünleştirilmeye çalışan kapsayıcı eğitim bu olumsuz davranışları ortadan kaldırmayı hedeflemektedir. "),
+      InfoCardItem("egitimkapsayici_info_2", "Tanım-2", "UNESCO tarafından kapsayıcı eğitim “öğrencilerin farklı ihtiyaçlarına, onların eğitime, kültüre ve sosyal hayata katılımlarını arttırarak ve ayrımcılığı en aza indirerek cevap verme süreci” olarak tanımlanmaktadır.\n-UNICEF’e göre kapsayıcı eğitim ise “tüm çocukların aynı okulda birlikte öğrenmeleridir."),
+      InfoCardItem("egitimkapsayici_info_3", "Tanım-3", "-Kapsayıcı eğitim dışlanmayı ve ayrımcılığı yok ederek herkese eşit, sosyal adaleti benimseyen, toplumsal değerlere ve toplumsal farklılıklara saygı duyan önemli değerleri vurgulayan bir eğitim anlayışıdır.\n-Bu anlayışın temelinde farklılıkların ortadan kaldırılarak aynılaştırma değil, farklılıkları olduğu gibi kabul edip saygı duyma olgusu yatar."),
+      InfoCardItem("egitimkapsayici_info_4", "Tanım-4", "Kapsayıcı eğitim; öğrencilerin gereksinim, ihtiyaç, tercih ve kişisel özellikleri analiz edilerek hazırlanmış planlı bir eğitim sürecidir. Böylece eğitim sürecinin niteliği ve öğrencilerin derse katılım ve ilgilileri artar."),
+    ]),InfoCardModel("Eğitimde Kapsayıcılık-2", [
+      InfoCardItem("egitimkapsayici_info_5", "Odaklandığı Temel Alanlar", "UNESCO (2009) yılında kapsayıcı eğitimin odaklandığı temel alanları şu şekilde belirlemiştir;\n-Sistem Kapsayıcılığı\n-Eğitime Erişim Kapsayıcılığı\n-Sosyal Kapsayıcılık"),
+      InfoCardItem("egitimkapsayici_info_6", "Sistem Kapsayıcılığı", "Eğitim sistemlerinin, okulların ve öğretmenlerin sosyal, kültürel ve bireysel çeşitliliğe saygı duyması, tüm öğrencilerin ihtiyaç ve beklentilerine cevap vermesi olarak tanımlanır."),
+      InfoCardItem("egitimkapsayici_info_7", "Eğitime Erişim Kapsayıcılığı", "Tüm bireyler için nitelikli eğitime eşit erişim olanağının sağlanması olarak tanımlanır."),
+      InfoCardItem("egitimkapsayici_info_8", "Sosyal Kapsayıcılık", "Sosyal politikalar ile tüm paydaşların beklentilerini de kapsayacak şekilde eğitim süreçleri dahilinde yakın koordinasyon içinde olunması olarak tanımlanır."),
+    ]),InfoCardModel("Eğitimde Kapsayıcılık-3", [
+      InfoCardItem("egitimkapsayici_info_5", "Kapsayıcı Eğitimin Gerekçeleri", "Kapsayıcı eğitimin gerekçeleri temel olarak 3 başlıkta toplanır. Bu başlıklar;\n-Eğitimsel gerekçe\n-Sosyal gerekçe\n-Ekonomik gerekçe"),
+      InfoCardItem("egitimkapsayici_info_6", "Eğitimsel gerekçe", "-Öğrencilerin kişisel özellik ve ihtiyaçlarına hitap eden eğitim programlarının geliştirilmesi gereklidir.\n-Bu şekilde geliştirilen eğitim programları ile öğrencilerin öğrenme potansiyelleri desteklenirken özgüvenleri de artacaktır."),
+      InfoCardItem("egitimkapsayici_info_7", "Sosyal gerekçe", "Öğrencilerin adaletli ve fırsat eşitsizliklerinden arınmış bir ortamda eğitim görmesi toplumsal bir değişikliğin temelini oluşturacaktır."),
+      InfoCardItem("egitimkapsayici_info_8", "Ekonomik gerekçe", "-Tüm öğrencilerin bir çatı altında eğitim görmesi farklı türde okullara olan ihtiyacı azaltacağı için düşük maliyetli olacaktır.\n-Ekonomik gerekçe öğrencilerin okula değil okulların öğrenciye uygun olması ilkesine dayanır."),
+      InfoCardItem("egitimkapsayici_info_9", "Temel İlkeler1-1", "-Öğrencilerin ihtiyaç ve gereksinimlerine uygun bir eğitim imkanı sunulmalıdır.\n-Eğitimde adalet ve fırsat eşitliği sağlanmalıdır.\n-Eğitim sürecine aile ve toplumun katılımı sağlanmalıdır."),
+      InfoCardItem("egitimkapsayici_info_10", "Temel İlkeler1-2", "-Öğrencilerin istihdamı için altyapı hazırlanmalıdır.\n-Öğrencilerin bilişsel, duyuşsal ve toplumsal becerilerinin geliştirilmesi amaçlanmalıdır.\n-Kapsayıcı eğitimin iyileştirilmesi için bilimsel çalışmalar yapılmalı, çalışmaların sonuçlarına uygun etkinlik ve uygulamalar hazırlanmalıdır."),
+    ]),InfoCardModel("Eğitimde Kapsayıcılık-4", [
+      InfoCardItem("egitimkapsayici_info_11", "Kapsayıcı Eğitimin Faydaları-1", "Kapsayıcı eğitimin, eğitim sitemine ve öğretim sürecine olan katkıları şu şekilde özetlenebilir;\n-Tüm çocukların öğrenmesini ve fırsat eşitliği elde etmesini hedefler.\n-Bireysel farklılıkların dikkate alındığı eğitim programı ile öğrencilerin öğrenme potansiyelleri desteklenir. Böylece öğrenciler özgüven kazanırlar."),
+      InfoCardItem("egitimkapsayici_info_12", "Kapsayıcı Eğitimin Faydaları-2", "-Bireysel farklılıkları dikkate alarak her öğrenci için uygun ders içeriği sunulur, her öğrenci kendini değerli ve özel hisseder.\n-Öğrencilerin ev, okul ve toplum yaşamını ilişkilendirir ve bütünleştirir."),
+      InfoCardItem("egitimkapsayici_info_13", "Kapsayıcı Eğitimin Faydaları-3", "-Öğrenciler tek yönlü bakış açılarını geliştirerek farklılıklara değer vermeye başlarlar.\n-Öğrencilerin okula ilgilisi, derslere katılımı artar ve devamsızlıkları azalır.\n-Öğrencilerin farklı gördükleri bireylere karşı önyargılarını azaltır, toplumsal barışa katkı sağlar ve toplumsal ortak duyguları geliştirir."),
+      InfoCardItem("egitimkapsayici_info_14", "Kapsayıcı Eğitimin Faydaları-4", "-Öğrenciler kendilerini daha geniş bir topluluğun parçası olarak görürler, sosyal ilişki kurmaları kolaylaşır ve empati yetenekleri gelişir.\n-Öğrencilerin, ait oldukları küçük grubun ötesinde, kendilerini daha geniş toplumun bir parçası olarak görmelerini sağlamak ve o toplumdaki diğer gruplardan bireyler ile empati ve ilişki kurmalarını sağlamak."),
+    ]),InfoCardModel("Eğitimde Kapsayıcılık-5", [
+      InfoCardItem("egitimkapsayici_info_15", "Kapsayıcı Eğitim Kapsamındaki Gruplar", "-Kapsayıcı eğitim ilk olarak sadece engelli öğrencilere yönelik ortaya çıkmıştır.\n-Daha sonraları ise tüm dezavantajlı grupları içine alacak şekilde genişlemiştir.\n-Bu dezavantajlı gruplar;\n-1.	Etnik Köken\n-Fiziksel Yetersizlik ve Özel Gereksinim\n-Sosyoekonomik Düzey\n-Dini İnanç\n-Cinsiyet"),
+      InfoCardItem("egitimkapsayici_info_16", "Etnik Köken", "-Öğrencilerin mülteci, göçmen, sığınmacı olmaları ya da etnik kökenleri sebebiyle okulda ve okul dışında olumsuz davranış (dışlama, aşağılama vb.) yaşamalarıdır."),
+      InfoCardItem("egitimkapsayici_info_17", "Fiziksel Yetersizlik ve Özel Gereksinim", "-Öğrencilerin özel durumları sebebiyle olumsuz davranışlara maruz kalma durumudur.\n-Bu öğrencileri sınıfta dışlanması, aşağılanması bu duruma örnek olarak verilebilir."),
+      InfoCardItem("egitimkapsayici_info_18", "Sosyoekonomik Düzey", "-Düşük ya da yüksek gelir durumu, aile yapısı vb. konulardan dolayı öğrencilerin olumsuz davranışlara maruz kalma durumudur."),
+      InfoCardItem("egitimkapsayici_info_19", "Dini İnanç", "Öğrencilerin inancı, ibadeti ya da mezhebi sebebiyle dışlanması durumudur."),
+      InfoCardItem("egitimkapsayici_info_20", "Cinsiyet", "Öğrencilerin cinsiyetleri sebebiyle yaşadığı ayrımcılık durumlarıdır. Kız öğrencilerin okuldan alınamaması ya da gönderilmemesi örnek olarak verilebilir."),
+    ]),InfoCardModel("Eğitimde Kapsayıcılık-6", [
+      InfoCardItem("egitimkapsayici_info_21", "", "Bu bölümde yer alan davranışlar, kapsayıcı eğitim ihtiyacı olan(Dışlanan, ayrımcılığa uğrayan vb.) çocuklarda sıklıkla görülen davranışlardır."),
+      InfoCardItem("egitimkapsayici_info_22", "", "-Öğrenci okulu sevmediğini söyler. Okula karşı ilgisizlik duyar ve okula gitmek istemeyebilir. \n-Öğrenci akranlarıyla, kendinden büyük ya da küçük çocuklarla göz teması ve iletişim kurmakta zorlanabilir."),
+      InfoCardItem("egitimkapsayici_info_23", "", "-Öğrenci okuldan eve geldiğinde yorgun, huysuz ve aksi davranışlarda bulunabilir.\n-Okuldan eve geldiği zaman okulda yaşadığı problemlerden bahsedebilir.\n-Öğrenci sanat, müzik, dans gibi güzel sanatlara olan ilgisini zamanla kaybedebilir.\n-Boş zaman aktivitelerini bırakabilir."),
+      InfoCardItem("egitimkapsayici_info_24", "", "-Okulla ilgili hiçbir şey yapmak istemeyebilir. Okul dışı etkinliklere yönelebilir. \n-Okul ile ilgili yapması gerekenleri (Ödev, proje vb.) son ana bırakır ya da yapmaz.\n-Okulla ilgili heyecan verici ya da mutlu olduğu durumları anlatmayı bırakabilir.\n-Okul rehberlik servisi ya da öğrencinin sınıf öğretmeni öğrencinin yaşadığı-yaşattığı problemlerle ilgili sizinle görüşmek isteyebilir."),    ]),
     /*InfoCardModel("", [
  InfoCardItem("egitimkapsayici_info_5", "", ""),
-      InfoCardItem("egitimkapsayici_info_6", "", ""),
-      InfoCardItem("egitimkapsayici_info_7", "", ""),
-      InfoCardItem("egitimkapsayici_info_8", "", ""),
-      InfoCardItem("egitimkapsayici_info_9", "", ""),
-      InfoCardItem("egitimkapsayici_info_10", "", ""),
-      InfoCardItem("egitimkapsayici_info_11", "", ""),
-      InfoCardItem("egitimkapsayici_info_12", "", ""),
-      InfoCardItem("egitimkapsayici_info_13", "", ""),
-      InfoCardItem("egitimkapsayici_info_14", "", ""),
-      InfoCardItem("egitimkapsayici_info_15", "", ""),
-      InfoCardItem("egitimkapsayici_info_16", "", ""),
-      InfoCardItem("egitimkapsayici_info_17", "", ""),
-      InfoCardItem("egitimkapsayici_info_18", "", ""),
-      InfoCardItem("egitimkapsayici_info_19", "", ""),
-      InfoCardItem("egitimkapsayici_info_20", "", ""),
-      InfoCardItem("egitimkapsayici_info_21", "", ""),
-      InfoCardItem("egitimkapsayici_info_22", "", ""),
-      InfoCardItem("egitimkapsayici_info_23", "", ""),
-      InfoCardItem("egitimkapsayici_info_24", "", ""),
+
+
+
+
+
+
       InfoCardItem("egitimkapsayici_info_25", "", ""),
       InfoCardItem("egitimkapsayici_info_26", "", ""),
       InfoCardItem("egitimkapsayici_info_27", "", ""),
@@ -85,5 +97,3 @@ class EgitimKapsayiciInfoViewModel extends ChangeNotifier{
       InfoCardItem("egitimkapsayici_info_7", "", ""),
     ])*/
   ];
-
-  List<InfoCardModel> get egitimKapsayici => _egitimKapsayiciList;}
