0aef4b6436c0ee6347ad3fd6b0a8c5b1099c2ff8
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2022-06-19 20:57:01 +0300

    added sosyal iletisim ve etkilesim info

--- lib/view_model/info_cards/sosyaletkilesim_info_view_model.dart
+++ lib/view_model/info_cards/sosyaletkilesim_info_view_model.dart
@@ -4,6 +4,8 @@ import '../../model/info_card_item.dart';
 import '../../model/info_card_model.dart';
 
 class SosyalEtkilesimInfoViewModel extends ChangeNotifier{
+  List<InfoCardModel> get sosyalEtkilesim => _sosyalEtkilesimList;}
+
   List<InfoCardModel> _sosyalEtkilesimList = [
     /* InfoCardModel("Hazırlanıyor", [
       InfoCardItem("Hazırlanıyor", "Hazırlanıyor", "Hazırlanıyor"),
@@ -46,32 +48,37 @@ class SosyalEtkilesimInfoViewModel extends ChangeNotifier{
       InfoCardItem("gg_sosyaletkilesim_info_27", "Geri Bildirim (Dönüt)-1", "-Geri bildirim, alıcının kaynaktan gelen iletiye gösterdiği her türlü tepkinin, kaynağa tekrar ulaşma sürecidir.\n-Geri bildirim hedefin kaynağa verdiği cevap olarak da tanımlanabilir.\n-Geri bildirim sayesinde kaynak mesajın anlaşılıp anlaşılamadığını öğrenir."),
       InfoCardItem("gg_sosyaletkilesim_info_28", "Geri Bildirim (Dönüt)-2", "-Mesajın tam olarak algılandığı zaman kaynağa iletilen bildirim olumlu geri bildirimdir.\n-Mesajın tam olarak algılanamadığı zaman ise kaynağa olumsuz geri bildirim sağlanır.\n-Geri bildirim yapılmadığı zaman ise tepkisizlik nedeniyle iletişim eksik kalmış olur."),
       InfoCardItem("gg_sosyaletkilesim_info_29", "Gürültü", "-İletişim sürecini olumsuz etkileyen ve engelleyen tüm unsurlar gürültü olarak adlandırılmaktadır.\n-Gürültü iletişim sürecinde istemsiz olarak ortaya çıkmaktadır.\n-Alıcı ve gönderici arasındaki mesajda oluşan bir fark söz konusu ise bunun sebebi gürültüdür.\n-Alıcının psikolojik durumu, televizyonun bozuk çıkan sesi, duyu organlarının yetersizliği gürültüye örnek verilebilir."),
-      //InfoCardItem("gg_sosyaletkilesim_info_30", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_31", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_32", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_33", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_34", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_35", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_36", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_37", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_38", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_39", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_40", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_41", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_42", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_43", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_44", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_45", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_46", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_47", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_48", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_49", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_50", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_51", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_52", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_53", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_54", "", ""),
-      //InfoCardItem("gg_sosyaletkilesim_info_55", "", ""),
+    ]), InfoCardModel("İletişimin Türleri-1", [
+      InfoCardItem("gg_sosyaletkilesim_info_30", "İletişim Türleri", "İletişim Türleri aşağıdaki gibi sıralanabilir.\n\n1-Birey-Birey Arasındaki İletişim\na.Psikolojik İletişim\nb.Sosyal İletişim\n\n2.Birey-Grup İletişimi\n3.Kitle İletişimi"),
+      InfoCardItem("gg_sosyaletkilesim_info_31", "Birey-Birey Arasındaki İletişim", "-Birey, başka bireyler ile iletişim kurabileceği gibi kendi içerisinde de iletişim süreci yaşayabilir.\n-İç dünyası ile iletişim de ilk iletişimidir.\n-Dış dünyada kurduğu iletişim, iç dünyasındaki iletişimin yansımasıdır.\n\nBirey-Birey Arasındaki İletişim ikiye ayrılır;\n-Psikolojik (Öz) İletişim\n-Sosyal (Kişiler Arası) İletişim"),
+      InfoCardItem("gg_sosyaletkilesim_info_32", "Psikolojik (Öz) İletişim-1", "-Bireyin kendisiyle olan iç iletişimini ifade eder.\n-Bir nevi kişinin içsel sorular sorup cevaplama sürecidir.\n-Kişinin düşünmesi, hayal kurması, duygulanması, ihtiyaçlarının farkına varması, olayları yorumlaması şeklinde gerçekleşir. Psikolojik iletişim en yoğun iletişim türüdür.\n-Birey bu zihinsel süreçte hem alıcı hem de kaynaktır. "),
+      InfoCardItem("gg_sosyaletkilesim_info_33", "Psikolojik (Öz) İletişim-2", "-Psikolojik iletişim kişinin çevresiyle kurduğu iletişimde belirleyici olmaktadır.\n-Kendisiyle kurduğu iletişimin dışa yansıması çevreyle kurduğu iletişimdir.\n-İnsanın iletişiminde içe yönelmesi, iletişim ihtiyacını başkaları ile karşılayamadığı zamanlarda daha sık görülür."),
+      InfoCardItem("gg_sosyaletkilesim_info_34", "Sosyal (Kişiler Arası) İletişim-1", "-İnsanın yaratılışı gereği iletişim kurmaya ve paylaşmaya ihtiyacı vardır.\n-İletişim kurma, insanların bir arada yaşamasının ve sosyal yapısının gereğidir.\n-Sosyal iletişim en az iki birey arasında gerçekleşen iletişim süreci olarak ifade edilir.\n-Bu süreçte kaynak ve hedef insanlardan oluşmaktadır.\n-Sürecin doğru ve etkin bir biçimde kurulmasına tamamlayıcı iletişim denir. "),
+      InfoCardItem("gg_sosyaletkilesim_info_35", "Sosyal (Kişiler Arası) İletişim-2", "-Tamamlayıcı iletişimin amacı, insanlar arası diyaloğu geliştirmek ve devamlılığı sağlamaktır.\n-Tamamlayıcı iletişim fertler arasındaki sorunların kaldırılmasını amaçlayan çok yönlü bir iletişim türüdür.\n-Kişiler arası iletişimin unsurları bir sonraki kartımızda verilecektir."),
+      InfoCardItem("gg_sosyaletkilesim_info_36", "Sosyal (Kişiler Arası) İletişim-3", "-Kişiler arası iletişimin üç temel esası vardır. Bu esaslardan biri bile eksikse kişiler arası iletişim kavramından söz etmek mümkün değildir. Bu esaslar; \n--Kişiler arası iletişimde kaynak ve hedef belli bir yakınlık derecesinde ilişki halinde olmalıdır. \n-Kişiler karşılıklı mesaj alışverişi ile geçen çift yönlü bir iletişim sürecinde olmalıdır. Sadece kaynağın etkin olduğu tek yönlü bir iletişim süreci söz konusu olamaz. \n- Kişiler arası mesajlar sözlü ve sözsüz nitelikte olmalıdır."),
+      InfoCardItem("gg_sosyaletkilesim_info_37", "Kişiler Arası İletişim Unsurları", "Kişiler arası iletişimde unsurları;\n\n-Di,\n-Dil Ötesi\n-Yüz ve Beden,\n-Bedensel temas\n-Mekân kullanımı,\n-Araçlar olarak sıralanabilir.\n\nŞimdi bu unsurların detaylarına bakalım"),
+      InfoCardItem("gg_sosyaletkilesim_info_38", "Dil", "Karşılıklı konuşma ve yazılı iletişim kurma bu gruba dahildir."),
+      InfoCardItem("gg_sosyaletkilesim_info_39", "Dil Ötesi", "-Kelimelerin nasıl söylendiği üzerinde durulur.\n-Ses tonu, ses şiddeti, vurgular, duraklamalar dil ötesi kavramlardan bazılarıdır."),
+      InfoCardItem("gg_sosyaletkilesim_info_40", "Yüz ve Beden", "Mimikler, beden dili, el hareketleri, göz teması vb. sözsüz iletişimde önemli yer tutar. "),
+      InfoCardItem("gg_sosyaletkilesim_info_41", "Bedensel temas", "-Sözsüz iletişim kapsamında temas ve dokunma yoluyla mesaj iletmektir.\n-Küçük bir çocuğun başının okşanması, yaşça büyük birinin elinin öpülmesi örnek olarak verilebilir.\n-Farklı kültürlerde farklı anlamlar taşıyabileceği için fiziksel temasa dikkat edilmelidir."),
+      InfoCardItem("gg_sosyaletkilesim_info_42", "Mekân kullanımı", "-Asansörde hiç tanımadığımız kişilerden olabildiğince uzaklaşırken, samimi olduklarımıza daha yakın dururuz.\n-Amir ya da patronların odasının daha büyük olması bu iletişim kapsamında değerlendirilebilir."),
+      InfoCardItem("gg_sosyaletkilesim_info_43", "Araçlar", "-Kişilere mesaj iletmek için kullanılan farklı yolları ifade eder.\n-Cenazede siyah giyinmek, rozet takmak, koku sürmek vb. örnek olarak verilebilir."),
+    ]),
+    InfoCardModel("İletişimin Türleri-2", [
+      InfoCardItem("gg_sosyaletkilesim_info_44", "Birey-Grup İletişimi-1", "-İletişim sürecinin önemli bir yanını grup iletişimi oluşturur.\n-Grup içi iletişimin temel amacı bireyler arasında etkileşimi sağlamaktır.\n-Gruplar, aralarındaki iletişimi yüz yüze kurabilecekleri gibi, iletişim araçlarından faydalanarak da kurabilirler.\n-Grup iletişimi belli amaçlar için bir araya gelmiş bireyler şeklinde ortaya çıkabilir."),
+      InfoCardItem("gg_sosyaletkilesim_info_45", "Birey-Grup İletişimi-2", "-Her birey grup içerisinde birbirine bağlıdır.\n-Gruplarda merkezi ve merkezi olmayan şeklinde iki çeşit iletişim vardır.\n-Merkezi grup iletişiminde, birey mesajı iletirken ya da alırken mutlaka bir rolü vardır.\n-Merkezi olmayan iletişimde ise birey herhangi bir rol üstlenmek zorunda değildir.\n-Mesaj serbestçe dolaşabilir."),
+      InfoCardItem("gg_sosyaletkilesim_info_46", "Grup İletişiminin Etkinliğinin Arttırılması-1", "-Grup üyesi iletişimin etkisini yitirmemesi için kelimeleri açık ve net bir şekilde telaffuz etmelidir.\n-Grup üyesi kendini öne çıkarmamalıdır. Sözleri tüm grup üyelerini ilgilendirmelidir.\n-Grup üyeleri iletişim sürecinde diğer üyelerle etkileşim içerisinde bulunmalıdır."),
+      InfoCardItem("gg_sosyaletkilesim_info_47", "Grup İletişiminin Etkinliğinin Arttırılması-2", "-Grup üyesinin söyledikleri grubun bir ihtiyacı ile bağlantılı olmalıdır.\n-Söylenenler sadece bir konu üzerinde yoğunlaşmalıdır.\n-Bu şekilde grup söylenenleri daha iyi anlayabilirler.\n-Söylenenler grup üyeleri ile aynı dilde ve öz şekilde yapılırsa iletişim çok daha etkili olur."),
+      InfoCardItem("gg_sosyaletkilesim_info_48", "Grup İletişimi Modelleri", "-Merkezi Model\n-Y Modeli\n-Zincir Modeli\n-Daire Modeli\n-Serbest Model"),
+      InfoCardItem("gg_sosyaletkilesim_info_49", "Merkezi Model", "-Otorite ve karar alma gücü hiyerarşik yapının en üstünde bulunanda toplanır. \n-Bireylerin çalışma tatmini azdır.\n-İletişimin hız ve doğruluk derecesi yüksektir.\n-Geleneksel organizasyon yapılarında çok sık görülür."),
+      InfoCardItem("gg_sosyaletkilesim_info_50", "Y Modeli", "-Az sayıda iletişim kanalı bulunmaktadır. \n-Y harfine benzeyen bir yapı kurulur.\n-Merkezileşme derecesi merkezi modelden sonra en fazla olandır.\n-Bireylerin çalışma tatmini azdır."),
+      InfoCardItem("gg_sosyaletkilesim_info_51", "Zincir Modeli", "-Zincir modeli iletişim sürecinde bireylerin yakınlık seviyelerine göre işlemektedir. \n-Grup verimliliği azalmaktadır.\n-Bireyler arasındaki iletişim zayıftır.\n-Bazı bireyler iletişim sürecime hiç katılmayabilirler."),
+      InfoCardItem("gg_sosyaletkilesim_info_52", "Daire Modeli", "-Belirli bir liderin olmadığı ve üyelerden herhangi birinin iletişimi başlatabildiği modeldir. \n-Merkezileşme derecesi azdır.\n-Bireylerin kendi aralarında iletişim olanakları fazladır."),
+      InfoCardItem("gg_sosyaletkilesim_info_53", "Serbest Model", "-Tüm iletişim kanallarının her zaman herkese açık olduğu bir durumdur.\n-Bireyler en ufak bir kısıtlama olmadan iletişim halindedirler.\n-Merkezileşme azdır.\n-Doğruluk derecesi düşüktür."),
+
+     ]),InfoCardModel("Kitle İletişimi", [
+      InfoCardItem("gg_sosyaletkilesim_info_54", "", ""),
+      InfoCardItem("gg_sosyaletkilesim_info_55", "", ""),
       //InfoCardItem("gg_sosyaletkilesim_info_56", "", ""),
       //InfoCardItem("gg_sosyaletkilesim_info_57", "", ""),
       //InfoCardItem("gg_sosyaletkilesim_info_58", "", ""),
@@ -87,12 +94,10 @@ class SosyalEtkilesimInfoViewModel extends ChangeNotifier{
       //InfoCardItem("gg_sosyaletkilesim_info_68", "", ""),
       //InfoCardItem("gg_sosyaletkilesim_info_69", "", ""),
       //InfoCardItem("gg_sosyaletkilesim_info_7", "", ""),
-    ]),
+     ]),
     InfoCardModel("Hazırlanıyor", [
       InfoCardItem("Hazırlanıyor", "Hazırlanıyor", "Hazırlanıyor"),
       InfoCardItem("Hazırlanıyor", "Hazırlanıyor", "Hazırlanıyor"),
       InfoCardItem("Hazırlanıyor", "Hazırlanıyor", "Hazırlanıyor"),
     ]),
   ];
-
-  List<InfoCardModel> get sosyalEtkilesim => _sosyalEtkilesimList;}
